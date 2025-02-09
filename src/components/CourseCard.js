8aecbd44f8a4fa83ec3512755d0066a63a0a977c
TobiasGrav <tobiagra@stud.ntnu.no>
2025-02-09 15:35:09 +0100

    Most popular courses now displayed on landing page

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,15 +1,19 @@
 import '../App.css';
 import { useNavigate } from "react-router-dom";
+import Rating from './Rating';
 
-function CourseCard({imgLink, title, creator}) {
+function CourseCard({id, imgLink, title, creator, rating, amount}) {
 
   const navigate = useNavigate();
 
   return (
-    <div className="Course-Card-Container" onClick={() => navigate("/course")}>
-      <img style={{height:'128px', maxWidth:'192px'}} src={imgLink} alt='CourseImage'/>
-      <text>{title}</text>
-      <text>{creator}</text>
+    <div className="Course-Card-Container" onClick={() => navigate(`course/${id}`)}>
+      <img style={{height:'128px', maxWidth:'100%', borderRadius:'10px 10px 0px 0px'}} src={imgLink} alt='CourseImage'/>
+      <div className='Course-Card-Info'>
+        <text>{title}</text>
+        <Rating rating={rating} amount={amount} light={false} />
+        <text>{creator}</text>
+      </div>
     </div>
   );
 }
--- src/components/CourseHierarchy.js
+++ src/components/CourseHierarchy.js
@@ -1,17 +1,55 @@
 import '../App.css';
 import CourseCard from './CourseCard';
+import { useNavigate } from "react-router-dom";
+import { Get } from '../utils/fetch';
+import { URL } from '../utils/url';
+import { useEffect, useState } from 'react';
 
 function CourseHierarchy({imgLink, title, creator}) {
+
+  const [listOfCourses, setListOfCourses] = useState([]);
+  const [hasRecievedResponse, setHasRecievedResponse] = useState(false);
+  
+  const navigate = useNavigate(URL.BACKEND, "api/");
+
+  useEffect(() => {
+
+    const fetchData = async () => {
+      let responseData = await Get("api/anonymous/mostPopularCourses?page:0&size:5");
+      setListOfCourses(responseData);
+    }
+
+    fetchData();
+  }, [])
+
+  useEffect(() => {
+    setHasRecievedResponse(true);
+    console.log(listOfCourses);
+  }, [listOfCourses])
+
+  const createCourseCards = () => {
+    return(
+      listOfCourses.map((course, index) => (
+        <CourseCard
+          id={course.id} // Ideally, use course.id if available
+          imgLink="https://foundr.com/wp-content/uploads/2023/04/How-to-create-an-online-course.jpg.webp"
+          title={course.courseName}
+          creator="WJC"
+          rating={course.averageRating}
+          amount={course.numberOfReviews}
+          price={course.price}
+        />
+      ))
+    )
+  }
+
   return (
     <div className="Course-Hierarchy-Container">
-        <h2>Top rated courses</h2>
-      <div className="Course-Container">
-        <CourseCard imgLink='https://foundr.com/wp-content/uploads/2023/04/How-to-create-an-online-course.jpg.webp' title="How to Win Every Argument (Even When You're Wrong)" creator='WJC'/>
-        <CourseCard imgLink='https://foundr.com/wp-content/uploads/2023/04/How-to-create-an-online-course.jpg.webp' title='JavaScript for People Who Hate JavaScript'          creator='NTNU'/>
-        <CourseCard imgLink='https://foundr.com/wp-content/uploads/2023/04/How-to-create-an-online-course.jpg.webp' title='Azure certification'                                creator='Microsoft, Azure'/>
-        <CourseCard imgLink='https://foundr.com/wp-content/uploads/2023/04/How-to-create-an-online-course.jpg.webp' title='The Art of Looking Busy While Doing Nothing'        creator='Valve Corporation'/>
-        <CourseCard imgLink='https://foundr.com/wp-content/uploads/2023/04/How-to-create-an-online-course.jpg.webp' title='Get Rich Quick (Or at Least Look Like You Are)'     creator='Finance bros'/>
+      <h2>Top rated courses</h2>
+      <div className='Course-Container'>
+        {hasRecievedResponse && createCourseCards()}
       </div>
+      <a href='' onClick={() => navigate("/search")}>View all popular courses</a>
     </div>
   );
 }
