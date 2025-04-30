cb9d55b5da0d10e4e39f9b697d7f2a2b7bf36968
TobiasGrav <tobiagra@stud.ntnu.no>
2025-04-30 12:47:38 +0200

    Made major stylistic changes, changed icons for correct sizing and improving performance score

---
+++ src/pages/CreateCoursePage.js
@@ -0,0 +1,14 @@
+import '../App.css';
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
+import CreateCourse from '../components/myCourses/ManageCourse';
+
+function CreateCoursePage() {
+  return (
+    <div className='App'>
+      <CreateCourse />
+    </div>
+  );
+}
+
+export default CreateCoursePage;
